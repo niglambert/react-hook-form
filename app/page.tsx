fc761ae68628019545028cea702f9a8c594a6d01
niglambert <nig_lambert@yahoo.com>
2025-03-16 10:18:05 +0000

    Added useState

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,61 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const timezones = ["GMT", "EST", "MST", "CST", "PST"];
+const timezoneList = ["GMT", "EST", "MST", "CST", "PST"];
 
-const roles = ["Dealer", "Price Approver", "Product Engineer"];
+const roleList = ["Dealer", "Price Approver", "Product Engineer"];
 
-const themes = ["Light", "Dark", "System"];
+const themeList = ["Light", "Dark", "System"];
 
 export default function Home() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [description, setDescription] = useState("");
+  const [timezone, setTimezone] = useState("");
+  const [theme, setTheme] = useState("");
+  const [roles, setRoles] = useState<string[]>([]);
+  const [joiningDate, setJoiningDate] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errors, setErrors] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (
+      !username ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !description ||
+      !timezone ||
+      !theme ||
+      !roles.length ||
+      !joiningDate
+    ) {
+      setErrors("Please fill out all the fields");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setErrors("Password and Confirm Password should match");
+      return;
+    }
+
+    setIsSubmitting(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setIsSubmitting(false);
+
+    setUsername("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    setDescription("");
+    setTimezone("");
+    setTheme("");
+    setRoles([]);
+    setJoiningDate("");
+    setErrors("");
   };
 
   return (
@@ -23,45 +69,56 @@ export default function Home() {
           {/* TEXT   */}
           <input
             type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             placeholder="Username"
             className=" text-gray-900 bg-white px-4 py-2 rounded"
           />
           {/* EMAIL   */}
           <input
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
             className=" text-gray-900 bg-white px-4 py-2 rounded"
           />
           {/* PASSWORD   */}
           <input
             type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
             className=" text-gray-900 bg-white px-4 py-2 rounded"
           />
           {/* CONFIRM PASSWORD   */}
           <input
             type="text"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             placeholder="Confirm Password"
             className=" text-gray-900 bg-white px-4 py-2 rounded"
           />
           {/* TEXTAREA   */}
           <input
             type="textarea"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
             placeholder="Description"
             className=" text-gray-900 bg-white px-4 py-2 rounded"
           />
 
           {/* SELECT   */}
           <select
-            value=""
+            value={timezone}
+            onChange={(e) => setTimezone(e.target.value)}
             className=" text-gray-900 bg-white px-4 py-2 rounded"
           >
             <option value="" className=" bg-white px-4 py-2 rounded" disabled>
               Select Timezone
             </option>
-            {timezones.map((unit) => (
-              <option key={unit} value={unit}>
-                {unit}
+            {timezoneList.map((timezone) => (
+              <option key={timezone} value={timezone}>
+                {timezone}
               </option>
             ))}
           </select>
@@ -69,15 +126,16 @@ export default function Home() {
           {/* RADIO   */}
           <div className="flex gap-2">
             <span className="font-semibold w-16">Theme</span>
-            {themes.map((theme) => (
-              <label key={theme}>
+            {themeList.map((themeValue) => (
+              <label key={themeValue}>
                 <input
                   type="radio"
-                  name="response"
-                  value={theme}
+                  value={themeValue}
+                  onChange={(e) => setTheme(e.target.value)}
+                  name="theme"
                   className="bg-white mr-1"
                 />
-                {theme}
+                {themeValue}
               </label>
             ))}
           </div>
@@ -85,14 +143,22 @@ export default function Home() {
           {/* CHECKBOX   */}
           <div className="flex gap-2">
             <span className="font-semibold w-16">Role</span>
-            {roles.map((role) => (
-              <label key={role}>
+            {roleList.map((roleValue) => (
+              <label key={roleValue}>
                 <input
                   type="checkbox"
-                  name="response"
                   className="bg-white mr-1"
+                  value={roleValue}
+                  onChange={(e) => {
+                    const { value, checked } = e.target;
+                    setRoles((prev) =>
+                      checked
+                        ? [...prev, value]
+                        : prev.filter((f) => f !== value),
+                    );
+                  }}
                 />
-                {role}
+                {roleValue}
               </label>
             ))}
           </div>
@@ -100,14 +166,20 @@ export default function Home() {
           {/* DATE */}
           <input
             type="date"
+            value={joiningDate}
+            onChange={(e) => setJoiningDate(e.target.value)}
             placeholder="Joining Date"
             className=" text-gray-900 bg-white px-4 py-2 rounded"
           />
 
           {/* BUTTON   */}
-          <button className=" text-white bg-blue-500 py-2 rounded hover:active:translate-y-[1px]">
+          <button
+            className=" text-white bg-blue-500 py-2 rounded disabled:bg-gray-500 disabled:pointer-events-none hover:active:translate-y-[1px]"
+            disabled={isSubmitting}
+          >
             Submit
           </button>
+          {errors && <div className="error text-red-500 py-2">{errors}</div>}
         </form>
       </div>
     </div>
